26884b1c98c3b23e500e0b32214b3ba13093a1d6
chhotu1 <chhotukumarsow@gmail.com>
2022-08-16 07:03:37 +0530

    add user role

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,15 +1,19 @@
 var Leave = require("./../models/Leave");
 var Holiday = require("./../models/Holiday");
+var Attendence = require("./../models/Attendence");
 const Response = require('./../utils/Response');
+const Constant = require('./../config/Constant')
 exports.dashboard = {
     dashboard: async function (req, res) {
         let userId = req.user._id;
         let role = req.user.role;
-        let total_leave = await Leave.find(role===1 || role===2?{}:{created_by: userId}).count();
+        let total_leave = await Leave.find(role===Constant.ADMIN || role===Constant.HR_MANEGER?{}:{created_by: userId}).count();
+        let total_attendence = await Attendence.find(role===Constant.ADMIN || role===Constant.HR_MANEGER?{}:{user_id: userId}).count();
         let total_holiday = await Holiday.find({}).count();
         let data ={
             total_leave,
-            total_holiday
+            total_holiday,
+            total_attendence
         }
         let result = { message: "Dashboard record", data:data, status: true };
         return res.json(Response.Response(result));

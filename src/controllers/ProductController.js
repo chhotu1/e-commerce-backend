f52479607a6cf87aa418074ddac025729c932131
chhotu1 <chhotukumarsow@gmail.com>
2022-09-11 23:16:30 +0530

    lookup used

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,9 +1,11 @@
 
+const mongoose = require("mongoose");
+
 const Product = require('./../models/Product');
 
 exports.Product = {
 	store: async function (req, res) {
-		const { title, category_id, phone, address,offer,offer_price,slug,price,discription,image_url,image_name } = req.body;
+		const { title, category_id, phone, address, offer, offer_price, slug, price, discription, image_url, image_name } = req.body;
 		let userId = req.user._id;
 		try {
 			let photo = '';
@@ -14,11 +16,11 @@ exports.Product = {
 				let sampleFile = req.files.photo[0];
 				let fileName = sampleFile.name.split(".");
 				fileName = fileName[0] + Date.now() + "." + fileName[1];
-				photo = '/src/uploads/products/'+fileName;
+				photo = '/src/uploads/products/' + fileName;
 				sampleFile.mv("./src/uploads/products/" + fileName, function (err) {
 					if (err) return res.json({
-						message:err,
-						status:false,
+						message: err,
+						status: false,
 					});
 				});
 				// if (req.files.photo) {
@@ -38,7 +40,7 @@ exports.Product = {
 				category_id,
 				phone, address,
 				created_by: userId,
-				image: photo!==''?photo:image_url,
+				image: photo !== '' ? photo : image_url,
 				offer_price,
 				offer,
 				slug,
@@ -55,14 +57,14 @@ exports.Product = {
 		}
 	},
 	list: async function (req, res) {
-		if( Object.keys(req.query).length !== 0){
-			await Product.find().sort({_id: -1}).limit(parseInt(req.query.page)).populate('created_by').populate('category_id').then(data => {
+		if (Object.keys(req.query).length !== 0) {
+			await Product.find().sort({ _id: -1 }).limit(parseInt(req.query.page)).populate('created_by').populate('category_id').then(data => {
 				return res.json({ status: true, success: true, statusCode: 200, data: data });
 			}).catch(error => {
 				return res.json({ status: false, success: true, statusCode: 40, data: error });
 			})
-		}else{
-			await Product.find().sort({_id: -1}).populate('created_by').populate('category_id').then(data => {
+		} else {
+			await Product.find().sort({ _id: -1 }).populate('created_by').populate('category_id').then(data => {
 				return res.json({ status: true, success: true, statusCode: 200, data: data });
 			}).catch(error => {
 				return res.json({ status: false, success: true, statusCode: 40, data: error });
@@ -70,18 +72,59 @@ exports.Product = {
 		}
 	},
 	showOne: async function (req, res) {
+		let obj={};
+		obj['_id'] =mongoose.Types.ObjectId(req.params.id);
 
-		if(!req.params.id) {
-			return res.json({ status: false, success: false,  data: '',message: "Products id can not be empty" + req.params.id});
-		}
-		await Product.findById(req.params.id).populate('created_by').populate('category_id').then(data => {
-			return res.json({ status: true, success: true, statusCode: 200, data: data });
-		}).catch(error => {
-			return res.json({ status: false, success: false, statusCode: 40, data: error });
-		})
+		const data = await Product.aggregate([
+			{
+				$match:{...obj}
+			},
+			{
+				$lookup: {
+					from: 'categories',
+					localField: 'category_id',
+					foreignField: '_id',
+					as: 'category'
+				}
+			},
+			{
+				$unwind: '$category'
+			},
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'created_by',
+					foreignField: '_id',
+					as: 'user'
+				}
+			},
+			{
+				$unwind: '$user'
+			},
+			{
+				$project: {
+					_id: 1,
+					price: 1,
+					discription: 1,
+					title: '$category.title',
+					name: '$user.name',
+				}
+			}
+		])
+		return res.json({ status: true, success: true, statusCode: 200, data: data });
+
+
+		// if(!req.params.id) {
+		// 	return res.json({ status: false, success: false,  data: '',message: "Products id can not be empty" + req.params.id});
+		// }
+		// await Product.findById(req.params.id).populate('created_by').populate('category_id').then(data => {
+		// 	return res.json({ status: true, success: true, statusCode: 200, data: data });
+		// }).catch(error => {
+		// 	return res.json({ status: false, success: false, statusCode: 40, data: error });
+		// })
 	},
 	update: async function (req, res) {
-		const { title, category_id, phone, address,offer,offer_price,slug,price,discription } = req.body;
+		const { title, category_id, phone, address, offer, offer_price, slug, price, discription } = req.body;
 		let userId = req.user._id;
 
 		// if(userId!==req.params.id){
@@ -90,65 +133,65 @@ exports.Product = {
 		// 		status:false,
 		// 	});
 		// }
-		
-		await Product.findByIdAndUpdate(req.params.id, {
-			title:title,
-			category_id:category_id,
-			phone:phone,
-			address:address,
-			offer_price:offer_price,
-			offer:offer,
-			slug:slug,
-			price:price,
-			discription:discription
-		}, {new: true})
-		.then(product => {
-			if(!product) {
-				return res.json({ status: false, success: false,  data: '',message: "product not found with id" + req.params.id});
-			}
-			return res.json({ status: true, data: product, message: 'Product Updated successfully' })
 
-		}).catch(err => {
-			if(err.kind === 'ObjectId') {
-				return res.json({ status: false, success: false,  data: '',message: "Products not found with id " + req.params.id });
-			}
-			return res.json({ status: false, success: false,  data: '',message: "Error updating product with id" + req.params.id });
-		});
+		await Product.findByIdAndUpdate(req.params.id, {
+			title: title,
+			category_id: category_id,
+			phone: phone,
+			address: address,
+			offer_price: offer_price,
+			offer: offer,
+			slug: slug,
+			price: price,
+			discription: discription
+		}, { new: true })
+			.then(product => {
+				if (!product) {
+					return res.json({ status: false, success: false, data: '', message: "product not found with id" + req.params.id });
+				}
+				return res.json({ status: true, data: product, message: 'Product Updated successfully' })
+
+			}).catch(err => {
+				if (err.kind === 'ObjectId') {
+					return res.json({ status: false, success: false, data: '', message: "Products not found with id " + req.params.id });
+				}
+				return res.json({ status: false, success: false, data: '', message: "Error updating product with id" + req.params.id });
+			});
 	},
 	delete: async function (req, res) {
-		if(!req.params.id) {
-			return res.json({ status: false, success: false,  data: '',message: "Products content can not be empty" + req.params.id});
+		if (!req.params.id) {
+			return res.json({ status: false, success: false, data: '', message: "Products content can not be empty" + req.params.id });
 		}
 		await Product.findByIdAndRemove(req.params.id)
-		.then(prod => {
-			if(!prod) {
-				return res.json({ status: false, success: false,  data: '',message: "Products not found with id " + req.params.id});
-			}
-			return res.json({ status: true, success: true,  data: '',message: "Products deleted successfully!"});
-		}).catch(err => {
-			if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-				return res.json({ status: false, success: false,  data: '',message: "Products not found with id " + req.params.id });
-            }
-			return res.json({ status: false, success: false,  data: '',message: "Could not delete Products with id " + req.params.id });
-		});
+			.then(prod => {
+				if (!prod) {
+					return res.json({ status: false, success: false, data: '', message: "Products not found with id " + req.params.id });
+				}
+				return res.json({ status: true, success: true, data: '', message: "Products deleted successfully!" });
+			}).catch(err => {
+				if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+					return res.json({ status: false, success: false, data: '', message: "Products not found with id " + req.params.id });
+				}
+				return res.json({ status: false, success: false, data: '', message: "Could not delete Products with id " + req.params.id });
+			});
 	},
 
 	deleteMultiple: async function (req, res) {
-		if(!req.body.ids) {
-			return res.json({ status: false, success: false,  data: '',message: "Products content can not be empty"});
+		if (!req.body.ids) {
+			return res.json({ status: false, success: false, data: '', message: "Products content can not be empty" });
 		}
-		await Product.remove({'_id':{'$in':req.body.ids.split(',')}})
-		.then(prod => {
-			if(!prod) {
-				return res.json({ status: false, success: false,  data: '',message: "Products not found with id "});
-			}
-			return res.json({ status: true, success: true,  data: '',message: "Products deleted successfully!"});
-		}).catch(err => {
-			if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-				return res.json({ status: false, success: false,  data: '',message: "Products not found with id " });
-            }
-			return res.json({ status: false, success: false,  data: '',message: "Could not delete Products with id " });
-		});
+		await Product.remove({ '_id': { '$in': req.body.ids.split(',') } })
+			.then(prod => {
+				if (!prod) {
+					return res.json({ status: false, success: false, data: '', message: "Products not found with id " });
+				}
+				return res.json({ status: true, success: true, data: '', message: "Products deleted successfully!" });
+			}).catch(err => {
+				if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+					return res.json({ status: false, success: false, data: '', message: "Products not found with id " });
+				}
+				return res.json({ status: false, success: false, data: '', message: "Could not delete Products with id " });
+			});
 	}
-	
+
 }
